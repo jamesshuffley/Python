959d18b142be98baffae08f7246b19ae511e8148
James Shuffley <jamesshuffley@hotmail.co.uk>
2022-05-19 11:43:39 +0100

    added variables_intro

---
+++ variables_intro/intro.py
@@ -0,0 +1,16 @@
+a = 1
+b = 2
+c = 3.5
+d = "Hello!"
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Pythion is a dynamically typed language (we don't tell it what a variable type is)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
