d6d2d2f8873779c4c07bbf9b3026c85bf5317fbc
James Shuffley <jamesshuffley@hotmail.co.uk>
2022-05-20 16:19:54 +0100

    modified fizzbuzz file

--- control_flow/fizzbuzz.py
+++ control_flow/fizzbuzz.py
@@ -7,13 +7,10 @@ while play:
     for i in range(101):
         if i % 3 == 0 and i % 5 == 0:
             print(f"{fizz}{buzz}")
-            continue
         elif i % 3 == 0:
             print(f"{fizz}")
-            continue
         elif i % 5 == 0:
             print(f"{buzz}")
-            continue
         print(i)
     again = str(input("Do you want to run the game again, type yes or no\n"))
     if again == "no":
