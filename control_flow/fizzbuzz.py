c9920b7093cfd9b0abeafad0d7a56a0d50fcc650
James Shuffley <jamesshuffley@hotmail.co.uk>
2022-05-21 12:38:08 +0100

    modified fizzbuzz to have working user input

--- control_flow/fizzbuzz.py
+++ control_flow/fizzbuzz.py
@@ -6,13 +6,24 @@ buzz = 'shuffley'
 while play:
     for i in range(101):
         if i % 3 == 0 and i % 5 == 0:
-            print(f"{fizz}{buzz}")
+            print(f'{fizz}{buzz}')
+            continue
         elif i % 3 == 0:
-            print(f"{fizz}")
+            print(f'{fizz}')
+            continue
         elif i % 5 == 0:
-            print(f"{buzz}")
+            print(f'{buzz}')
+            continue
         print(i)
-    again = str(input("Do you want to run the game again, type yes or no\n"))
-    if again == "no":
-        play = False
-        print('Thank you for running the game')
+    answer = None
+    while answer not in ("yes", "no"):
+        answer = input("Do you want to run the game again, type yes or no\n")
+        if answer == "yes":
+            break
+        elif answer == "no":
+            print('Thank you for running the game')
+            play = False
+        else:
+            print("sorry that is not a valid response, please type yes or no\n")
+
+
